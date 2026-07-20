09b9b4d67ff5ceb212ee9f4a71e095745833536c
tanay ranka collage <tanayranka42@gmail.com>
2026-07-20 20:50:12 +0530

    Create Question 4.cpp

---
+++ Expiriment 2/Question 4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Student{
+    public:
+    int roll, marks;
+    string name;
+    void accept();
+    void display();
+};
+// accesing using Scope Resolution Operator
+void Student::accept(){
+    cout << "Enter roll no: ";
+    cin >> Student::roll;
+    cout << "Enter marks: ";
+    cin >> Student::marks;
+    cout << "Enter name: ";
+    cin >> Student::name;
+}
+
+void Student::display(){
+    cout << endl;
+    cout << "Name: " << Student::name << endl;
+    cout << "Marks: " << Student::marks << endl;
+    cout << "Roll No.: " << Student::roll << endl;
+}
+int main() {
+    Student f;
+    f.accept();
+    f.display();
+    return 0;
+}
