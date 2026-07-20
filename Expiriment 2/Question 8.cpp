44e745a6c94e57d5304c87bb887cf51dce0cf22f
tanay ranka collage <tanayranka42@gmail.com>
2026-07-20 20:54:07 +0530

    Create Question 8.cpp

---
+++ Expiriment 2/Question 8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "enter length: ";
+    cin >> n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++){
+        cout << "Enter for " << i+1 << " element: ";
+        cin >> arr[i];
+    }
+    cout << endl;
+    for(int i=0;i<n;i++){
+        cout << " " << arr[i] << endl;
+    }
+    delete[] arr;    //deleting the array
+    return 0;
+}
