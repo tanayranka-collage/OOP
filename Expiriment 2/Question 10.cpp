c1e0f2fb248852e3f84bbc98ac0a4df7684bba3a
tanay ranka collage <tanayranka42@gmail.com>
2026-07-20 21:16:10 +0530

    Create Question 10.cpp

---
+++ Expiriment 2/Question 10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+      int phy,eng,maths,bio,it;
+      cout << "Enter marks for Physics: ";
+      cin >> phy;
+      cout << "Enter marks for English: ";
+      cin >> eng;
+      cout << "Enter marks for Maths: ";
+      cin >> maths;
+      cout << "Enter marks for Biology: ";
+      cin >> bio;
+      cout << "Enter marks for Inforamtion Technology: ";
+      cin >> it;
+
+  // Display Logic with manipulators:
+
+
+
+    return 0;
+}
