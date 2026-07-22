2343ecdac6e3e5290365f6bfa11e5d485d4822a5
tanay ranka collage <tanayranka42@gmail.com>
2026-07-22 15:24:27 +0530

    Update Question 4.cpp

--- Expiriment 3/Question 4.cpp
+++ Expiriment 3/Question 4.cpp
@@ -1,1 +1,12 @@
 //code by tanay ranka sycse b 8
+//explicit typecasting int to float and vice versa
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 69;
+    float gg = 21.234;
+    
+    cout << "Int to float: " << static_cast<float>(num) << endl;
+    cout << "Float to int: " << static_cast<int>(gg) << endl;
+    return 0;
+}
