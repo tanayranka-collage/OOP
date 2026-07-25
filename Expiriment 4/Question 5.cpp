2fe806bdfd2bdb2e8923ced0c6dcda220c5e4717
tanay ranka collage <tanayranka42@gmail.com>
2026-07-25 11:44:05 +0530

    Update Question 5.cpp

--- Expiriment 4/Question 5.cpp
+++ Expiriment 4/Question 5.cpp
@@ -1,1 +1,24 @@
 // code by tanay ranka sycse b 8
+#include<iostream>
+using namespace std;
+class rectangle{
+    private:
+    float l,b,Area;
+    public:
+    void area();
+};
+inline void rectangle::area(){
+    cout << "Enter Length: ";
+    cin >> l;
+    cout << "enter Breth: ";
+    cin >> b;
+    cout << endl;
+    Area = l*b;
+    cout << "Area of ractangle is: " << Area << endl;
+    
+}
+int main(){
+    rectangle e;
+    e.area();
+    return 0;
+}
