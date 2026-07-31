6154e1dfaa751ff6e98f213e829921bfdf9b051a
tanay ranka collage <tanayranka42@gmail.com>
2026-07-31 14:55:49 +0530

    Create Question 3.cpp

---
+++ Expiriment 5/Question 3.cpp
@@ -0,0 +1,25 @@
+//code by tanay ranka sycse b 8
+#include <iostream>
+#include<iomanip>
+using namespace std;
+class Circle{
+    private:
+    int r;
+    const double pi = 3.1415280923;
+    public:
+    void accept(){
+        cout << "Enter radius: ";
+        cin >> r;
+    }
+    void area();
+};
+
+inline void Circle::area(){
+    cout << "Area of circle is: " << setprecision(6) << pi * r * r << endl;
+}
+int main() {
+    Circle p;
+    p.accept();
+    p.area();
+    return 0;
+}
